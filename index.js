5a79bfc957844e2a477c56c993111fd5d482756e
Yogas <ramayogaz@gmai.com>
2021-10-24 21:20:11 +0800

    Added search toggle JS

---
+++ index.js
@@ -0,0 +1,6 @@
+let search = document.getElementById("search");
+let searchBar = document.getElementById("searchBar");
+
+search.addEventListener("click", function () {
+  searchBar.classList.toggle("searchbar");
+});
